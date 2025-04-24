e408ede067b99131c86a8735fa23a5beabb425f3
harinivash8 <hari.v8080@gmail.com>
2025-04-24 20:35:19 +0530

    Create install_docker.sh

---
+++ scripts/install_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Installing Docker..."
+
+# Install Docker (Amazon Linux 2 version)
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Optional: Enable Docker to start on boot
+sudo systemctl enable docker
+
+echo "Docker installed and started."
